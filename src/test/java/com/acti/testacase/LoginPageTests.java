384465e19d97e797e9d9ce8c1f43f5392f8387dc
shantosh <shantosh25@hotmail.com>
2022-11-29 08:39:12 +0530

    Moved the common code to a BaseTest Class

--- src/test/java/com/acti/testacase/LoginPageTests.java
+++ src/test/java/com/acti/testacase/LoginPageTests.java
@@ -2,57 +2,41 @@ package com.acti.testacase;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import com.acti.base.DriverScript;
-import com.acti.pages.EnterPage;
-import com.acti.pages.LoginPage;
 
-public class LoginPageTests extends DriverScript {
+public class LoginPageTests extends BaseTest {
 	
 	@Test
 	public void testLoginPageTitle()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	{		
 		String expected = "actiTIME - Login";
 		String actual = lp.getLoginPageTitle();
-		Assert.assertEquals(actual, expected);
-		quitDriver();
+		Assert.assertEquals(actual, expected);	
 	}
 	
 	@Test
 	public void testActiImageIsDisplayed()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	{	
 		boolean flag = lp.verifyActiLogo();
 		Assert.assertTrue(flag);
-		quitDriver();
 	}
 	
 	@Test
 	public void testForgotPasswordIsDisplayed()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	{		
 		boolean flag = lp.verifyForgotPassword();
 		Assert.assertTrue(flag);
-		quitDriver();
 	}
 	
 	@Test
 	public void testLoginFunction()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	{	
 		lp.enterUsername("admin");
 		lp.enterPassword("manager");
 		lp.clickLogin();
-		EnterPage ep = new EnterPage();
 		String expected = "John Doe";
 		String actual = ep.verifyUserLoggedIn();
 		Assert.assertEquals(actual, expected);
-		ep.clickLogout();
-		quitDriver();
+		ep.clickLogout();	
 	}
 
 }
