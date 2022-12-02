1e0cab8ad5d57db63f5d2e03bb040c89df9cec90
shantosh <shantosh25@hotmail.com>
2022-12-02 08:20:50 +0530

    Added code for screen shot

--- src/main/java/com/acti/utils/Helper.java
+++ src/main/java/com/acti/utils/Helper.java
@@ -1,6 +1,18 @@
 package com.acti.utils;
 
-public class Helper {
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import com.acti.base.DriverScript;
+
+public class Helper extends DriverScript {
+	
+	// This method is used as a wait statement in some places
 	
 	public static void sleep()
 	{
@@ -12,6 +24,21 @@ public class Helper {
 		{	
 			e.printStackTrace();
 		}
+	}
+	
+	// This method is used to take the screen shot
+	
+	public static String captureScreenshot(WebDriver driver)
+	{
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String screenpath = "C:\\Users\\User\\git\\com.acti.automation\\atreports\\screens\\screen.png";
+		try {
+			FileHandler.copy(src, new File(screenpath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return screenpath;
 	}
 
 }
